517eeac2d80180996a9f050868d676bbbb9ad517
Prathameshds <132325224+Prathameshds@users.noreply.github.com>
2026-07-30 22:39:03 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ import { createServer as createViteServer } from 'vite';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(cors());
   app.use(express.json());
